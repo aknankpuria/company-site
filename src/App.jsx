90b786c973319ee0a477c28e2d509155b6fd7570
Jasmeet Kaur <131550417+jasmeetk17@users.noreply.github.com>
2023-08-04 16:02:00 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Affilated from "./components/Affilated";
 import Header from "./components/Header/Header";
 import Home from "./components/Home";
-import Testimonial from "./components/Testimonial";
+import Testimonial from "./components/testimonial";
 import Footer  from "./components/Footer";
 
 
--- src/components/Header/Services/Study Abroad/Abroad.jsx
+++ src/components/Header/Services/Study Abroad/Abroad.jsx
@@ -3,8 +3,56 @@ import React from 'react'
 const Abroad = () => {
   return (
     <div>
-        <p>Are you dreaming of studying abroad, exploring new cultures, and gaining a world-class education? At Goyal Associates, we believe that every aspiring student deserves the opportunity to fulfill their academic dreams, regardless of financial constraints. Our study loans are tailored to support students like you in pursuing your passions and ambitions on the global stage. Let's embark on this exciting journey together, making your dreams a reality with Goyal Associates Study Loans.</p></div>
+
+      <div>
+      <p>Are you aspiring to study abroad and looking for a quick and hassle-free way to secure an unsecured educational loan? Look no further! At Goyal Associates, we understand the urgency and importance of your dream to study overseas. That's why we offer unsecured educational loans that can be sanctioned within 72 hours, giving you the financial freedom to pursue your global education without delay.</p>
+      </div>
+
+
+     <div>
+     Why Choose Unsecured Educational Loans:
+<ol>
+<li>No Collateral Required: Unlike traditional secured loans, our unsecured educational loans do not require any collateral or security. This means you can access funds without putting your assets at risk.</li>
+
+<li>Fast Approval Process: We value your time and aim to provide quick loan approvals. Our streamlined process ensures that your application is processed swiftly, getting you the financial support you need within 72 hours.</li>
+
+<li>Simplified Documentation: We understand that applying for a loan can be daunting. Our minimal documentation requirements make the process stress-free and straightforward.</li>
+
+<li>Flexibility and Convenience: With no collateral involved, you have the flexibility to use the loan amount for various education-related expenses, including tuition fees, accommodation, travel, and study materials.</li>
+</ol>  
+   </div>
+
+   <div class="wrapper">
+        
+        <section class="contact" id="contact">
+       <form action="#contact">
+        <h2 className="heading slide-right">Contact <span> Us</span></h2>
+            <div class="input-box slide-left">
+                <input type="text" placeholder="Full-Name" />&nbsp;&nbsp;
+                <input type="email" placeholder="Email Address"/>
+
+            </div>
+            <div class="input-box slide-right">
+                <input type="date" placeholder="Enter"/>&nbsp;&nbsp;
+                <input type="number" placeholder="Annual Income"/>
+                    
+            </div>
+            <div class="input-box slide-left">
+                <input type="number" placeholder="Mobile Number"/>&nbsp;&nbsp;
+                
+                    
+            </div>
+        
+            <div >
+            <input type="submit" value="Send Message" class="btn  fade-in"/>
+            </div>
+        </form>
+        </section>
+   </div>
+   </div>
+
+    
   )
 }
 
-export default Abroad
+export default Abroad;
