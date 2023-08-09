a6a53e1b26736444aed634f9aca6b64d8c041bf0
Aslam Khan shah <aksi34012@gmail.com>
2023-08-10 02:36:35 +0530

    setup tailwind ]

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home";
 import Testimonial from "./components/testimonial";
 import Footer  from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+
 
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+       <Navbar />
      <Home/>
+   
      <Affilated/>
      <Testimonial/>
      <Footer/>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Navbar = () => {
+  return (
+<div>
+    <header>this is header section</header>
+  <div className="flex flex-row">
+  <div className="basis-1/4">01</div>
+  <div className="basis-1/4">02</div>
+  <div className="basis-1/2">03</div>
+</div>
+
+</div>
+  )
+}
+
+export default Navbar
